276e1bf7819a8e88d9aba5b38331755c5649e0e8
“AyushKava” <ayush2081@gmail.com>
2025-04-15 17:34:54 +0530

    Add api path also frontend path

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "http://localhost:1111",
-    baseURL: "https://your-backend.onrender.com",
+    baseURL: "https://react-js-project-server.onrender.com",
     withCredentials: true,
 })
 
